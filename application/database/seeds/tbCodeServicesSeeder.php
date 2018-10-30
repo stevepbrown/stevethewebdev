c3a486d020b77b5248711b2a6e41faa2a158e28c
Steve Brown <8r0wn5t3r@gmail.com>
2018-10-30 17:33:53 +0000

    tbCodeServicesSeeder working

--- application/database/seeds/tbCodeServicesSeeder.php
+++ application/database/seeds/tbCodeServicesSeeder.php
@@ -11,6 +11,75 @@ class tbCodeServicesSeeder extends Seeder
      */
     public function run()
     {
-        //
+        DB::table('tbCodeServices')->insert(
+            [
+                [
+                'service_name' => 'Template & websites',
+                'active' => 1
+                ],
+
+                [
+                'service_name' => 'Mobile-first sites',
+                'active' => 1
+                ],
+        
+                [
+                'service_name' => 'Bespoke Design & Build',
+                'active' => 1
+                ],
+            
+                [
+                'service_name' => 'Booking forms',
+                'active' => 0
+                ],
+            
+                [
+                'service_name' => 'Accelerated Mobile Pages (AMP)',
+                'active' => 0
+                ],
+            
+                [
+                'service_name' => 'Accessible sites',
+                'active' => 0
+                ],
+            
+                [
+                'service_name' => 'Social Media Integration',
+                'active' => 1
+                ],
+            
+                [
+                'service_name' => 'Copy writing',
+                'active' => 0
+                ],
+            
+                [
+                'service_name' => 'Content Managment Systems (CMS)',
+                'active' => 0
+                ],
+            
+                [
+                'service_name' => 'E-commerce',
+                'active' => 0
+                ],
+            
+                [
+                'service_name' => 'Photography',
+                'active' => 0
+                ],
+            
+                [
+                'service_name' => 'Web Analytics',
+                'active' => 0
+                ],
+            
+                [
+                'service_name' => 'Search engine optimisation (SEO)',
+                'active' => 1
+                ],
+            
+                
+            ]
+            );         
     }
 }
