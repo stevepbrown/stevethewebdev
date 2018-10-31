829e53726f2d32809c4b9bf016f3cf01534250f2
Steve Brown <8r0wn5t3r@gmail.com>
2018-10-31 13:33:19 +0000

    Created page controller

---
+++ application/app/Http/Controllers/PageController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Services;
+
+
+class PageController extends Controller
+{
+    
+    
+    
+    
+    public function show() {
+
+
+
+    }
+}
