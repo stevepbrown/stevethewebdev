634c3772809c3d25580ded029b0d8b6d18da2eb2
Steve Brown <8r0wn5t3r@gmail.com>
2018-11-01 15:18:55 +0000

    Added page controller

--- application/app/Http/Controllers/PageController.php
+++ application/app/Http/Controllers/PageController.php
@@ -9,13 +9,24 @@ use App\Services;
 
 class PageController extends Controller
 {
-    
-    
-    
-    
-    public function show() {
 
+    /**
+     * A collection of active online services 
+     *
+     */
+    protected $services;
+    
+    
+    public function __construct(Services $services)
+    {
+                
+        $this->services = $services->where('active','=',1)->get();
+    }
+    
+    
+    public function index() {
 
+        return view('pages.home')->with('title','Welcome');
 
     }
 }
