d2676d6d6ce5ce8e70d7cd06ad4543cd2948824d
Steve Brown <8r0wn5t3r@gmail.com>
2018-11-02 14:54:37 +0000

    Services model getActive method - WIP

--- application/app/Service.php
+++ application/app/Service.php
@@ -6,7 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Service extends Model
 {
-    
-   
-   
+     public function getActive(){
+
+        $active_services = $this->where('active','=',1);
+        
+        
+        return $active_services;
+
+    }
 }
