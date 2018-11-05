f09d713724d3be23216856b0cb13bcac25670299
Steve Brown <8r0wn5t3r@gmail.com>
2018-11-05 15:28:59 +0000

    Migrations & seeders for Pages table

---
+++ application/database/seeds/PagesSeeder.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class PagesSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('pages')->insert(
+            [
+                [
+                'title' => 'Home',
+                'slug' => null
+                 ],
+                              
+                
+                ['title' => 'Services',
+                'slug' => 'services'
+                 ],
+                              
+                [
+                'title' => 'Contact',
+                'slug' => 'contact'
+                 ],
+                              
+                [
+                'title' => 'Templates',
+                'slug' => 'templates'
+                 ],
+                              
+                [
+                'title' => 'Trades',
+                'slug' => 'trades'
+                ]
+              
+            ]
+                );                
+               
+    }
+}
