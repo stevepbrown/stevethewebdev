dce846637a477e6b8e43ea504173170f3b14f27c
Steve Brown <8r0wn5t3r@gmail.com>
2018-11-05 14:39:01 +0000

    Added slug to ServicesSeeder & ran successfully

--- application/database/seeds/ServicesSeeder.php
+++ application/database/seeds/ServicesSeeder.php
@@ -15,67 +15,80 @@ class ServicesSeeder extends Seeder
             [
                 [
                 'name' => 'Templated websites',
-                'active' => 1
+                'active' => 1,
+                'slug' =>'templates'
                 ],
 
                 [
                 'name' => 'Mobile-first sites',
-                'active' => 1
+                'active' => 1,
+                'slug' =>'mobile'
                 ],
         
                 [
                 'name' => 'Bespoke Design & Build',
-                'active' => 1
+                'active' => 1,
+                'slug' =>'bespoke'
                 ],
             
                 [
                 'name' => 'Booking forms',
-                'active' => 0
+                'active' => 0,
+                'slug' =>'bookings'
                 ],
             
                 [
                 'name' => 'Accelerated Mobile Pages (AMP)',
-                'active' => 0
+                'active' => 0,
+                'slug' =>'AMP'
                 ],
             
                 [
                 'name' => 'Accessible sites',
-                'active' => 0
+                'active' => 0,
+                'slug' =>'accessible'
                 ],
             
                 [
                 'name' => 'Social Media Integration',
-                'active' => 1
+                'active' => 1,
+                'slug' =>'social'
                 ],
             
                 [
                 'name' => 'Copy writing',
-                'active' => 0
+                'active' => 0,
+                'slug' =>'copy'
                 ],
             
                 [
                 'name' => 'Content Managment Systems (CMS)',
-                'active' => 0
+                'active' => 0,
+                'slug' =>'cms'
                 ],
             
                 [
                 'name' => 'E-commerce',
-                'active' => 0
+                'active' => 0,
+                'slug' =>'ecommerce'
                 ],
             
                 [
                 'name' => 'Photography',
-                'active' => 0
+                'active' => 0,
+                'slug' =>'photography'
                 ],
             
                 [
                 'name' => 'Web Analytics',
-                'active' => 0
+                'active' => 0,
+                'slug' =>'analytics'
                 ],
             
                 [
                 'name' => 'Search engine optimisation (SEO)',
-                'active' => 1
+                'active' => 1,
+                'slug' =>'seo'
                 ],
             
                 
