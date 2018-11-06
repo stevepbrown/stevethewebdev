6bf43da7eb28c94e72ee2bd755891a7d721d60ec
Steve Brown <8r0wn5t3r@gmail.com>
2018-11-06 13:45:18 +0000

    getRouteKeyName added to Page model

--- application/app/Page.php
+++ application/app/Page.php
@@ -6,5 +6,21 @@ use Illuminate\Database\Eloquent\Model;
 
 class Page extends Model
 {
-    //
+//   
+
+/**
+ * getRouteKeyName()
+ *
+ * @return string
+ * 
+ * If you would like model binding to use a database column other than id when 
+ * retrieving a given model class, you may override the getRouteKeyName method on the 
+ * Eloquent model:
+ */
+public function getRouteKeyName()
+{
+    return 'slug';
+}
+
+
 }
