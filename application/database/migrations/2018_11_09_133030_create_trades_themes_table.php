9485e16ae31aff308cd646b0c7ff735f3a451414
Steve Brown <8r0wn5t3r@gmail.com>
2018-11-09 15:36:45 +0000

    Trade_themes migration & seeder

---
+++ application/database/migrations/2018_11_09_133030_create_trades_themes_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTradesThemesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *:roll
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('trades_themes', function (Blueprint $table) {
+            
+            $table->increments('id');
+            $table->integer('theme_id')->foreign('theme_id')->references('id')->on('themes');
+            $table->integer('trade_id')->foreign('trade_id')->references('id')->on('trades');
+            $table->boolean('active');
+            $table->timestamps();
+            $table->unique(['theme_id', 'trade_id']);
+            
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('trade_themes');
+    }
+}
