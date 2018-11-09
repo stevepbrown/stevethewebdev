bdbb801f3846d6682283f4def0833a6c0ffd30d8
Steve Brown <8r0wn5t3r@gmail.com>
2018-11-09 18:03:25 +0000

    Theme model relations -WIP

--- application/app/Theme.php
+++ application/app/Theme.php
@@ -6,5 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Theme extends Model
 {
-    //
+    public function trades() {          
+             return $this->belongsToMany('App\Trade');
+          }
+
+      
 }
