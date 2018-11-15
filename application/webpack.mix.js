8be8c7ab90281d0f57a3d406211cb4e06a40c975
Steve Brown <8r0wn5t3r@gmail.com>
2018-11-15 16:20:53 +0000

    Restructuring related to ITCSS - settings-bootstrap-overrides - WIP

--- application/webpack.mix.js
+++ application/webpack.mix.js
@@ -1,136 +1,106 @@
+/***
+ *       ___           __ _                    _   _          
+ *      / __|___ _ _  / _(_)__ _ _  _ _ _ __ _| |_(_)___ _ _  
+ *     | (__/ _ \ ' \|  _| / _` | || | '_/ _` |  _| / _ \ ' \ 
+ *      \___\___/_||_|_| |_\__, |\_,_|_| \__,_|\__|_\___/_||_|
+ *                         |___/                              
+ */
+
+
+ 
+
+// Require mix  
 let mix = require('laravel-mix');
 
+// This snippet specifies that webpack should prepend var $ = require('jquery') //to every location that it encounters either the global $ identifier, or //window.jQuery. Nifty!
+mix.autoload({
+	jquery: ['$', 'window.jQuery']
+ });
+
+// Set the public path (the root for emitted code)
 mix.setPublicPath('./resources/assets');
 
+
 /***
- *         _                         _      _
- *      _ | |__ ___ ____ _ _____ _ _(_)_ __| |_
+ *         _                         _      _   
+ *      _ | |__ ___ ____ _ _____ _ _(_)_ __| |_ 
  *     | || / _` \ V / _` (_-< _| '_| | '_ \  _|
  *      \__/\__,_|\_/\__,_/__|__|_| |_| .__/\__|
- *                                    |_|
- */
-
-// Define the vendor js file locations
-
-	// Modules file path
-	var $node = './node_modules';
-
-	// Popper file path
-	var $popper_js = $node+'/popper.js/dist/popper.js';
-
-	// Jquery file path
-	var $jquery_js = $node+'/jquery/dist/jquery.js';
-
-	// Bootstrap file path
-	var $bootstrap_js = $node+'/bootstrap/dist/js/bootstrap.js';
-
-// Copy the vendor js to a common folder
-
-
-	// Define the laravel folder (for uncompiled js)
-	$laravel_js = 'resources/assets/js';
-
-	// Copy popper
-	mix.copy($popper_js, $laravel_js);
-
-	// Copy jquery
-	mix.copy($jquery_js, $laravel_js);
-
-	// copy bootstrap
-	mix.copy($bootstrap_js, $laravel_js);
-
-// Compile into a single vendor.js
-
-
-mix.babel([
-
-	($laravel_js+'/popper.js'),
-	($laravel_js+'/jquery.js'),
-	($laravel_js+'/bootstrap.js')
-
-	],
-
-		'/js/compiled/vendor.js');
-
-
-
-		/***
-		 *       ___ ___ ___
-		 *      / __/ __/ __|
-		 *     | (__\__ \__ \
-		 *      \___|___/___/
-		 *
-		 */
-
-
-		// Copy any vendor css files
-		mix.copy(
-		  [
-		    $node+'/font-awesome/css/font-awesome.css',
-		  ],
-		  './resources/assets/css');
-
-
-
-		/***
-		 *      ___   _   ___ ___
-		 *     / __| /_\ / __/ __|
-		 *     \__ \/ _ \\__ \__ \
-		 *     |___/_/ \_\___/___/
-		 *
-		 */
-
-		 /* NB.SASS files feature import statements from node modules  */
-
-		 // Compile the SASS
-		 mix.sass('./resources/assets/sass/default.scss', './resources/assets/sass/compiled/')
-		 .options({
-         processCssUrls: false
-     });
-
-
-/***
- *       ___ ___  __  __ ___ ___ _  _   _ _____ ___ ___  _  _
- *      / __/ _ \|  \/  | _ )_ _| \| | /_\_   _|_ _/ _ \| \| |
- *     | (_| (_) | |\/| | _ \| || .` |/ _ \| |  | | (_) | .` |
- *      \___\___/|_|  |_|___/___|_|\_/_/ \_\_| |___\___/|_|\_|
+ *                                    |_|       
  *
+ *  
  */
 
-	//CSS
-	mix.combine(['./resources/assets/sass/compiled/default.css','./resources/assets/css/font-awesome.css'],'./resources/assets/css/combined/combined.css');
+// Using Babel & vendor extraction to seperate vendor files and application's js
 
 
-		 /***
-		  *      __  __ _      _  __ _         _   _
-		  *     |  \/  (_)_ _ (_)/ _(_)__ __ _| |_(_)___ _ _
-		  *     | |\/| | | ' \| |  _| / _/ _` |  _| / _ \ ' \
-		  *     |_|  |_|_|_||_|_|_| |_\__\__,_|\__|_\___/_||_|
-		  *
-		  *
-		  *     !This only runs when the production command is used!
-		  *
-		  */
-			// css
-			mix.minify('./resources/assets/css/combined/combined.css','./resources/assets/css/combined/combined.min.css')
-
-			// js - issues encountered because of ES2015 compatibility (babel failure also)
-
-			/***
-			 *      ___ ___ ___ _____ ___ ___ ___ _   _ _____ ___ ___  _  _
-			 *     |   \_ _/ __|_   _| _ \_ _| _ ) | | |_   _|_ _/ _ \| \| |
-			 *     | |) | |\__ \ | | |   /| || _ \ |_| | | |  | | (_) | .` |
-			 *     |___/___|___/ |_| |_|_\___|___/\___/  |_| |___\___/|_|\_|
-			 *
-			 */
-
-			 // Both standard & minified
-
-			 	//css
-			 	mix.copy('./resources/assets/css/combined/combined.css','../public/css');
-			 	mix.copy('./resources/assets/css/combined/combined.min.css','../public/css');
+// mix.babel(
+// 	[
+// 		'app.js'
+		
+// 	]
+	
+// 	, './js')
+	
+	
+	mix.extract(
+		[
+			'jquery',
+			'bootstrap'
+			]
+			
+			);
+		
 
 
-				// js
-				mix.copy('./resources/assets/js/compiled/vendor.js','../public/js');
-				mix.copy('./resources/assets/js/custom.js','../public/js');
+
+/*
+ |--------------------------------------------------------------------------
+ | Mix Asset Management
+ |--------------------------------------------------------------------------
+ |
+ | Mix provides a clean, fluent API for defining some Webpack build steps
+ | for your Laravel application. By default, we are compiling the Sass
+ | file for your application, as well as bundling up your JS files.
+ |
+ */
+
+
+
+// Full API
+// mix.js(src, output);
+// mix.react(src, output); <-- Identical to mix.js(), but registers React Babel compilation.
+// mix.preact(src, output); <-- Identical to mix.js(), but registers Preact compilation.
+// mix.coffee(src, output); <-- Identical to mix.js(), but registers CoffeeScript compilation.
+// mix.ts(src, output); <-- TypeScript support. Requires tsconfig.json to exist in the same folder as webpack.mix.js
+// mix.extract(vendorLibs);
+// mix.sass(src, output);
+// mix.standaloneSass('src', output); <-- Faster, but isolated from Webpack.
+// mix.fastSass('src', output); <-- Alias for mix.standaloneSass().
+// mix.less(src, output);
+// mix.stylus(src, output);
+// mix.postCss(src, output, [require('postcss-some-plugin')()]);
+// mix.browserSync('my-site.test');
+// mix.combine(files, destination);
+// mix.babel(files, destination); <-- Identical to mix.combine(), but also includes Babel compilation.
+// mix.copy(from, to);
+// mix.copyDirectory(fromDir, toDir);
+// mix.minify(file);
+// mix.sourceMaps(); // Enable sourcemaps
+// mix.version(); // Enable versioning.
+// mix.disableNotifications();
+// mix.setPublicPath('path/to/public');
+// mix.setResourceRoot('prefix/for/resource/locators');
+// mix.autoload({}); <-- Will be passed to Webpack's ProvidePlugin.
+// mix.webpackConfig({}); <-- Override webpack.config.js, without editing the file directly.
+// mix.babelConfig({}); <-- Merge extra Babel configuration (plugins, etc.) with Mix's default.
+// mix.then(function () {}) <-- Will be triggered each time Webpack finishes building.
+// mix.extend(name, handler) <-- Extend Mix's API with your own components.
+// mix.options({
+//   extractVueStyles: false, // Extract .vue component styling to file, rather than inline.
+//   globalVueStyles: file, // Variables file to be imported in every component.
+//   processCssUrls: true, // Process/optimize relative stylesheet url()'s. Set to false, if you don't want them touched.
+//   purifyCss: false, // Remove unused CSS selectors.
+//   uglify: {}, // Uglify-specific options. https://webpack.github.io/docs/list-of-plugins.html#uglifyjsplugin
+//   postCss: [] // Post-CSS options: https://github.com/postcss/postcss/blob/master/docs/plugins.md
+// });
