5fb1ea48ab946104ff1f1704243381364b66b6a6
Steve Brown <8r0wn5t3r@gmail.com>
2018-11-25 18:05:01 +0000

    All services listed in services menu are rendering (view enhancement -WIP)

--- application/app/Http/Controllers/ServiceController.php
+++ application/app/Http/Controllers/ServiceController.php
@@ -4,27 +4,26 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
-use App\Page;
+use App\Service;
 
 class ServiceController extends Controller
 {
     
-    protected $page;
+    protected $service;
     protected $slug;
 
-    public function __construct(Request $request,Page $pages)
+    public function __construct(Request $request,Service $services)
     {
               
         $this->slug = $request->query('service');
-
-        
-        $this->page = $pages->where('slug','=',$this->slug)->firstOrFail();
+        $this->service = $services->where('slug','=',$this->slug)->firstOrFail();
         
     }
 
     public function index() {
            
-        return view('pages.'.$this->page->slug)->with('title',$this->page->title);
+       
+        return view('pages.service')->with('service',($this->service));
      
     }
 
--- application/resources/views/pages/service.blade.php
+++ application/resources/views/pages/service.blade.php
@@ -1,11 +1,15 @@
 @extends('layouts.main')
 
+{{-- This is used in the head partial --}}
+{{$title = $service->name}}
+
 @section('main')
 <div class="card">
   <i class="fa fa-mobile fa-4x mx-auto" aria-hidden="true"></i>
-  <h5 class="text-center">Service Name</h5>
+  <h5 class="text-center">{{$title}}</h5>
   <div class="card-body">
-      <p class="card-text"> service content text</p>
+          {{-- {{dd('content.content_services_'.$service->slug)}} --}}
+          <p class="card-text"> @include('content.content_services_'.$service->slug)</p>
   </div>
 </div>
 @endsection
@@ -13,5 +17,5 @@
 @section('scripts')
   @parent
   {{-- Additional scripts would go here  --}}
-  <script src="js/custom.js"></script>
+  {{-- <script src="js/custom.js"></script> --}}
 @endsection
