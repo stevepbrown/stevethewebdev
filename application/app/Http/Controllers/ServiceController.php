97c9fc1fd9f05a1461202fb70e477133a5751175
Steve Brown <8r0wn5t3r@gmail.com>
2018-11-25 16:59:04 +0000

    Added Service Controller
    Added templates page

--- application/app/Http/Controllers/ServiceController.php
+++ application/app/Http/Controllers/ServiceController.php
@@ -1,39 +1,33 @@
 <?php
 
 namespace App\Http\Controllers;
+
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Page;
 
-
-class PageController extends Controller
+class ServiceController extends Controller
 {
-
     
     protected $page;
     protected $slug;
-    
-    /**
-     * __construct
-     *
-     * @param Request $request
-     * @param Page $pages
-     */
+
     public function __construct(Request $request,Page $pages)
     {
-                
-        $this->slug=(($request->slug == null)? 'home': $request->slug);
+              
+        $this->slug = $request->query('service');
+
         
         $this->page = $pages->where('slug','=',$this->slug)->firstOrFail();
         
     }
-    
-    
+
     public function index() {
-       
+           
         return view('pages.'.$this->page->slug)->with('title',$this->page->title);
      
     }
-    
+
+
 
 }
