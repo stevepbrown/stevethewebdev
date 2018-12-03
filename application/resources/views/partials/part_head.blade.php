8cf0f52776e9b510681e2bbaf73618c6b945a54e
Steve Brown <8r0wn5t3r@gmail.com>
2018-12-03 14:59:54 +0000

    Add robots in env check for head partial

--- application/resources/views/partials/part_head.blade.php
+++ application/resources/views/partials/part_head.blade.php
@@ -6,6 +6,13 @@
     <meta name="author" content="Steve Brown">
     <title> {{ $title }}</title>
 
+    {{-- Do not index pages which are not live! --}}
+    @if ((env('APP_ENV'))!=='live')
+        
+        <meta name="robots" content="none"> 
+
+    @endif
+
     {{-- Add description for SEO --}}
     <meta name="description" content="" />
 
