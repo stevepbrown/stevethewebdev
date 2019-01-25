e046337a6e2a4e3535550c2f8d234f8871555eae
Steve Brown <8r0wn5t3r@gmail.com>
2019-01-25 14:03:01 +0000

    Wireframe slider operational

--- application/resources/views/pages/index.blade.php
+++ application/resources/views/pages/index.blade.php
@@ -16,28 +16,53 @@
 
 
 {{-- Note that here we're using grid to position elements dynamically, and so the order and relative
-placement of elements is independant of the order in which the appear here! --}}
+placement of elements is independant of the order in which the appear here!
+
+Uses 'z' classes to arrange placement on z-axis
+
+--}}
 @section('content')
 <div class="container-fluid grid-A">
-  <header>THIS IS YOUR HEADER
-      
-  </header>
-  <nav>THIS IS YOUR NAV</nav>
-  <main>THIS IS YOUR MAIN</main>
-  {{-- <footer>THIS IS YOUR FOOTER</footer> --}}
-  <div id="div-footer-partial">FOOTER PARTIAL</div>
-  <div id="div-footer-extended">FOOTER EXTENDED</div>
-  {{-- <div class="carousel">CAROUSEL</div> --}}
-  {{-- <section class="content">CONTENT SECTION</section> --}}
- 
-  {{-- <div id="div-brand"></div> --}}
-  {{-- <h1></h1> --}}
-  {{-- <h1>THIS IS YOUR H1</h1> --}}
-  {{-- <main></main> --}}
-  {{-- <div class="carousel"></div> --}}
-  {{-- <div id="div-content-container"></div> --}}
-  {{-- <div id="div-footer-partial"></div> --}}
-  {{-- <div id="div-footer-extended"></div> --}}
+      <header class="z0">THIS IS YOUR HEADER</header>
+      <main class="z0 container-fluid"></main>
+      <div id="carouselExampleIndicators" class="z1 carousel slide" data-ride="carousel">
+          <ol class="z1 carousel-indicators">
+            <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+            <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+            <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+          </ol>
+          <div class="carousel-inner">
+            <div class="carousel-item active">
+              {{-- <img class="d-block w-100" src="..." alt="First slide"> --}}
+              <div class="display-4 text-center">Slide 1</div>
+            </div>
+            <div class="carousel-item">
+              {{-- <img class="d-block w-100" src="..." alt="Second slide"> --}}
+              <div class="display-4 text-center">Slide 2</div>
+            </div>
+            <div class="carousel-item text-center">
+              {{-- <img class="d-block w-100" src="..." alt="Third slide"> --}}
+              <div class="display-4 text-center">Slide 3</div>
+            </div>
+          </div>
+          <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+          </a>
+          <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+          </a>
+        </div>
+      <section id="z1 section-company-info">THIS IS YOUR SECTION</section>
+      <footer class="z0">THIS IS YOUR FOOTER</footer> 
+      <div class="z1" id="div-brand">THIS IS YOUR BRAND</div>
+      <nav class="z1">THIS IS YOUR NAV</nav>
+      <h1 class="z1">THIS IS YOUR H1</h1>
+      <div class="z1" id="div-footer-partial">
+        @include('partials.part_footer')
+      </div>
+      <div class="z1" id="div-footer-extended">FOOTER EXTENDED</div>
 </div>
   @endsection
 
--- application/resources/views/partials/part_footer.blade.php
+++ application/resources/views/partials/part_footer.blade.php
@@ -1,5 +1,5 @@
 
-  <div class="container-fluid">
+  {{-- <div class="container-fluid">
     <div class="row">
       <div class="col-xs-12 col-md-12 text-center">Mobile: 07729 673 542</div>
 @if ($title !== 'Contact')
@@ -10,4 +10,15 @@
     <div class="row">
       <div class="col-12 text-center"> &copy; Steve Brown  {{ Carbon\Carbon::now()->format('Y')}} </div>
     </div>
+  </div> --}}
+  <div id="div-footer-partial" class="container">
+    <p class="text-center">Mobile: 07729 673 542</p>
+    @if ($title !== 'Contact')
+      <div class="col-xs-12 col-md-12 text-nowrap text-center">Email: steve@stevethewebdev.co.uk</div>
+    @endif
+    <p class="text-center">
+        &copy; Steve Brown  {{ Carbon\Carbon::now()->format('Y')}}
+    </p>
   </div>
+
+
