aea08dfc515d3e9c9c95ad0212c5d03c7e8dc0d9
Steve Brown <8r0wn5t3r@gmail.com>
2019-01-28 16:27:41 +0000

    Slider cards - WIP

--- application/resources/views/pages/index.blade.php
+++ application/resources/views/pages/index.blade.php
@@ -45,33 +45,69 @@ Uses 'z' classes to arrange placement on z-axis
       <li id="li-indicator-copy" data-target="#carouselServices" data-slide-to="4"></li>
       <li id="li-indicator-AMP" data-target="#carouselServices" data-slide-to="5"></li>
     </ol>
-    <div class="carousel-inner">
-      <div id="div-slide-welcome" class="jumbotron carousel-item active">
-        <h2 class="display-1 text-center">Welcome</h2>
-        <span class="lead">@include('content.content_subtitle')</span>
+<div class="carousel-inner" data-pause="hover">
+  <div id="div-slide-welcome" class="jumbotron carousel-item active">
+    <h2 class="display-1 ">Welcome</h2>
+  <span class="lead">@include('content.content_subtitle')</span>
+  </div>
+  <div id="div-slide-mobile" class="container-fluid carousel-item card">
+    <div class="card-header text-center">
+      <i class="fa fa-mobile fa-4x"></i>
+      <h3 class="display-4">Mobile-first</h3>
+    </div>
+    <div class="card-body">
+      @include('content.content_services_mobile')
+    </div>
+  </div>
+
+  <div id="div-slide-bespoke" class="container-fluid carousel-item card">
+      <div class="card-header text-center">
+        <i class="fa fa-cubes fa-4x"></i>
+        <h3 class="display-4">Design</h3>
       </div>
-      <div id="div-slide-mobile" class="container-fluid carousel-item">
-        <h3 class="display-2 text-center">Mobile-first</h3>
-        @include('content.content_services_mobile')
-      </div>
-      <div id="div-slide-bespoke" class="container-fluid carousel-item">
-        <h3 class="display-2 text-center">Design</h3>
+      <div class="card-body">
         @include('content.content_services_bespoke')
       </div>
-      <div id="div-slide-accessible" class="container-fluid carousel-item">
-        <h3 class="display-2 text-center">Accessibility</h3>
+    </div>
+
+  <div id="div-slide-accessible" class="container-fluid carousel-item card">
+      <div class="card-header text-center">
+        <i class="fa fa-universal-access fa-4x"></i>
+        <h3 class="display-4">Accessibility</h3>
+      </div>
+      <div class="card-body">
         @include('content.content_services_accessibility')
       </div>
-      
-      <div id="div-slide-copy" class="container-fluid carousel-item">
-        <h3 class="display-2 text-center">Copywriting</h3>
+    </div>
+
+   <div id="div-slide-X" class="container-fluid carousel-item card">
+      <div class="card-header text-center">
+        <i class="fa fa-pencil fa-4x"></i>
+        <h3 class="display-4">Copy</h3>
+      </div>
+      <div class="card-body">
         @include('content.content_services_copy')
       </div>
-      <div id="div-slide-AMP" class="container-fluid carousel-item">
-        <h3 class="display-2 text-center">Accelerated Mobile Pages</h3>
-        @include('content.content_services_amp_summary')
-      </div>
     </div>
+  
+
+  {{-- <div id="div-slide-AMP" class="container-fluid carousel-item">
+    <i class="fa fa-X fa-4x"></i>
+    <h3 class="display-5">Accelerated Mobile Pages</h3>
+    @include('content.content_services_amp_summary')
+  </div> --}}
+
+  <div id="div-slide-X" class="container-fluid carousel-item card">
+      <div class="card-header text-center">
+        <i class="fa fa-X fa-4x"></i>
+        <h3 class="display-4">X</h3>
+      </div>
+      <div class="card-body">
+        @include('content.content_services_X')
+      </div>
+  </div>
+
+</div>
     <a class="carousel-control-prev" href="#carouselServices" role="button" data-slide="prev">
       <span class="carousel-control-prev-icon" aria-hidden="true"></span>
       <span class="sr-only">Previous</span>
