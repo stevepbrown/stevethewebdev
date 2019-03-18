2836a2210c129462541e3463ba59835b96846dd8
Steve Brown <8r0wn5t3r@gmail.com>
2019-03-18 16:54:39 +0000

    location component /  mapping js - WIP

---
+++ application/resources/views/components/component_location_map.blade.php
@@ -0,0 +1,19 @@
+<div id="div-location-map">
+<style>
+    #map {
+      width: 100%;
+      height: 400px;
+      background-color: grey;
+    }
+   </style>
+
+<script>
+
+var $api = {{env('GEO_GOOGLE_MAPPING_API')}}
+var $src = "https://maps.googleapis.com/maps/api/js?key=".$api."&callback=initMap"
+
+
+
+</script>
+
+</div>
